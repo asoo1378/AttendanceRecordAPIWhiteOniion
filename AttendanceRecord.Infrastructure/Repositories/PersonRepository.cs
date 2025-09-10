2005f4ccac8ee9757fadafc9ed7e08e60200b904
Asoo <asoo.fatahi@gmail.com>
2025-09-10 21:13:37 +0430

    Add Synchronous to PersonRepository

--- AttendanceRecord.Infrastructure/Repositories/PersonRepository.cs
+++ AttendanceRecord.Infrastructure/Repositories/PersonRepository.cs
@@ -20,6 +20,7 @@ namespace AttendanceRecord.Infrastructure.Repositories
             _context = context;
         }
 
+        #region Add
         public void Add(Person person)
         {
             _context.Persons.Add(person);
@@ -31,21 +32,49 @@ namespace AttendanceRecord.Infrastructure.Repositories
             await _context.Persons.AddAsync(person);
             await _context.SaveChangesAsync();
         }
+        #endregion
 
+        #region List
+        public IEnumerable<Person> GetAll()
+        {
+            return _context.Persons.ToList(); 
+        }
+        
         public async Task<IEnumerable<Person>> GetAllAsync()
         {
             return await _context.Persons.ToListAsync();
+        }
+        #endregion
+
+        #region Get Id
+        public Person GetById( int id)
+        {
+            return _context.Persons.Find(id);
         }
 
         public async Task<Person> GetByIdAsync(int id)
         {
             return await _context.Persons.FindAsync(id);
         }
+        #endregion
+
+        #region Update
+        public void Update(Person person)
+        {
+            _context.Persons.Update(person);
+        }
 
         public async Task UpdateAsync(Person person)
         {
             _context.Persons.Update(person);
             await _context.SaveChangesAsync();
+        }
+        #endregion
+
+        #region Delete
+        public void Delete(Person person)
+        {
+            _context.Persons.Remove(person);
         }
 
         public async Task DeleteAsync(int id)
@@ -57,6 +86,13 @@ namespace AttendanceRecord.Infrastructure.Repositories
                 await _context.SaveChangesAsync();
             }
         }
+        #endregion
+
+
+        public int SaveChanges()
+        {
+            return _context.SaveChanges();
+        }
 
         public async Task<int> SaveChangesAsync()
         {
@@ -65,17 +101,7 @@ namespace AttendanceRecord.Infrastructure.Repositories
         }
 
 
-
-        public void Update(Person person)
-        {
-            _context.Persons.Update(person);
-        }
-
-        public void Delete(Person person)
-        {
-            _context.Persons.Remove(person);
-        }
-
+        
 
     }
 }
