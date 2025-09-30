35b9c0596b2bf18d83b1b78171ae05f7105ef17b
Asoo <asoo.fatahi@gmail.com>
2025-09-30 12:21:51 +0330

    Reflection in Services

---
+++ AttendanceRecord.Infrastructure/Extantions/ServiceInjectionExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace AttendanceRecord.Infrastructure.Extantions
+{   
+    public static class ServiceInjectionExtensions
+        {
+        public static void  AddServices(this IServiceCollection services , Assembly assembly)
+        {
+            var types = assembly.GetTypes().Where(t => t.Name.EndsWith("Service"));
+
+
+            foreach (var impl in types)
+            {
+                var interfaceType = impl.GetInterfaces().FirstOrDefault(i => i.Name == $"I{impl.Name}");
+                if (interfaceType == null)
+                {
+                    services.AddScoped(interfaceType, impl);
+
+                }
+
+            }
+                
+
+        }
+    }
+}
---
+++ AttendanceRecord.Infrastructure/Extantions/zxcdsdvsdfdfscd.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using Microsoft.Extensions.Options;
+
+namespace AttendanceRecord.Infrastructure.Extantions
+{
+    public static class zxcdsdvsdfdfscd
+    {
+        public static void AddServices(this IServiceCollection services, Assembly assembly)
+        {
+            var Types = assembly.GetTypes().Where(t => t.Name.EndsWith("Service"));
+
+            foreach (var Impl in Types)
+            {
+                var InterfaceType = Impl.GetInterfaces().FirstOrDefault(i => i.Name == $"I{Impl.Name}");
+
+                if (InterfaceType == null)
+                {
+                    services.AddScoped(InterfaceType, Impl);
+                }
+            }
+
+
+        }
+
+    }
+}
